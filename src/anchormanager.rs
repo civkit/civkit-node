66abb5840290375b9c5278e540840db61a85fada
Thomas Shelton <twshelton@hrtoolbox.com>
2023-06-28 08:07:52 -0400

    adding ability to create anchor manager with board pubkey and append offers to collection

--- src/anchormanager.rs
+++ src/anchormanager.rs
@@ -20,19 +20,44 @@ use bitcoin::secp256k1::{PublicKey};
 // use civkit::events::ClientEvents;
 // use civkit::kindprocessor::KindProcessor;
 
-pub struct AnchorManager {}
-
-pub struct SerializedOffers {
-    pub offers: [u64;10] //???
+pub struct AnchorManager {
+    our_service_pubkey: Arc<PublicKey>, 
+    our_service_offers: Arc<SerializedOffers>,     
 }
 
-impl SerializedOffers {}
+pub struct SerializedOffers {
+    offers: Vec<SerializedOffer>
+}
+
+pub struct SerializedOffer {
+    offer: String,
+    meta: String
+}
+
+impl SerializedOffers {
+    pub fn new() -> Self {
+        SerializedOffers {
+            offers: Vec::new()
+        }
+    }
+    pub fn push(&mut self, serialized_offer: SerializedOffer) -> () {
+        self.offers.push(serialized_offer)
+    }
+}
 
 impl AnchorManager {
-	// pub fn new(board_pubkey: Arc<PublicKey>, serialized_offers: Arc<SerializedOffers>) -> Self {
-	pub fn new() -> Self {
-    AnchorManager {}
+	pub fn new(board_pubkey: Arc<PublicKey>) -> Self {
+    let offers = SerializedOffers::new();
+    AnchorManager {
+        our_service_pubkey: board_pubkey,
+        our_service_offers: Arc::new(offers)
+    }
 	}
+
+  pub fn add_payload(&mut self, serialized_offer: Arc<SerializedOffer>) -> () {
+    let offer = Arc::clone(&serialized_offer);
+    //self.our_service_offers.push(&offer)
+  }
 
 	fn commit_note(&self) {}
 }
