b06db5b21798c018f85ab3c6609006bd075e7419
daven <daven.savoie@kureapp.com>
2023-08-10 06:25:44 -0400

    rebuilt from latest main to resolve merge issue

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,7 @@ pub struct Config {
     pub spam_protection: SpamProtection,
     pub connections: Connections,
     pub civkitd: Civkitd,
+    pub logging: Logging,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize)]
@@ -32,4 +33,36 @@ pub struct Civkitd {
     pub noise_port: i32,
     pub nostr_port: i32,
     pub cli_port: i32,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize)]
+pub struct Logging {
+    pub level: String,
+}
+
+// default config to fallback
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            performance: Performance {
+                max_db_size: 10000,
+                max_event_age: 3600,
+            },
+            spam_protection: SpamProtection {
+                requestcredentials: true,
+            },
+            connections: Connections {
+                maxclientconnections: 100,
+            },
+            civkitd: Civkitd {
+                network: "testnet".to_string(),
+                noise_port: 9735,
+                nostr_port: 50021,
+                cli_port: 50031,
+            },
+            logging: Logging {
+                level: "info".to_string(),
+            },
+        }
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -6,6 +6,8 @@ use std::path::PathBuf;
 use std::convert::Infallible;
 use tokio::sync::mpsc;
 use std::io::Write; 
+use std::fs;
+
 
 pub fn get_default_data_dir() -> PathBuf {
     let home_dir = dirs::home_dir().expect("Home directory not found");
@@ -29,16 +31,27 @@ pub fn get_default_data_dir() -> PathBuf {
 }
 
 // Function to initialize the logger with the given data directory
-pub fn init_logger(data_dir: &PathBuf) -> Result<(), Box<dyn Error + Send + Sync>> {
+pub fn init_logger(data_dir: &PathBuf, log_level: &str ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    
+    if !data_dir.exists() {
+        fs::create_dir_all(data_dir)?;
+    }
+    
     let log_file = data_dir.join("debug.log");
-
     let config = ConfigBuilder::new().build();
-    let log_writer = File::create(&log_file)?;
-    let log_writer = WriteLogger::new(LevelFilter::Trace, config.clone(), log_writer);
+    let level_filter = match log_level {
+        "error" => LevelFilter::Error,
+        "warn" => LevelFilter::Warn,
+        "info" => LevelFilter::Info,
+        "debug" => LevelFilter::Debug,
+        "trace" => LevelFilter::Trace,
+        _ => panic!("Invalid log level in config"),
+    };
 
-    CombinedLogger::init(vec![
-        TermLogger::new(LevelFilter::Info, config.clone(), TerminalMode::Mixed).unwrap(),
-        log_writer,
-    ])
-    .map_err(|err| Box::new(err) as Box<dyn Error + Send + Sync>)
+    let log_writer = File::create(&log_file)?;
+    let file_logger = WriteLogger::new(level_filter, config.clone(), log_writer);
+    let term_logger = TermLogger::new(level_filter, config, TerminalMode::Mixed).unwrap();
+
+    CombinedLogger::init(vec![file_logger, term_logger])
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send + Sync>)
 }
